35e53e9b7d0235b5b9b9bfbb37a449b6847e5321
Nadeesha Dilruwan <eclayaz@gmail.com>
2019-11-04 18:52:41 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// 'react' is the module, React is the component
+
+//projects created with create-react-app
+//when even changes are saves, application is automatically restarted
+//no need to refresh the browser, hot module replacement (HMR) do that for us
+
 //Babel will compile this to React.createElement
 //that's why we have imported React on the top
 //even though it's directly not used in the code
 const element = <h1>Hello World</h1>;
-console.log(element);
-
-//projects created with create-react-app
-//when even changes are saves, application is automatically restarted
-//no need to refresh the browser, hot module reloading
+ReactDOM.render(element, document.getElementById('root'));
+//in a real world app, instead of rendering single element, will render app component
+//app component is the root component of the application
